00a9b273a0abd7cca5a4a715f8c52c5d53b27cb2
Fay Alawadhi <fayalawadhi@Fays-MacBook-Pro.local>
2019-09-26 18:29:30 +0300

    done

---
+++ age_calculator.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+def check_birthdate(year, month, day): 
+	today = datetime.now()
+	
+
+# def check_birthdate():
+# 	year = input("Enter year of birth: ")
+# 	month = input("Enter month of born: ")
+# 	day = input("Enter day of born: ")
+
+# 	import datetime
+# 	x = datetime.datetime.now()
